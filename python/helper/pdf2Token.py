f91bed6acbc20c5a27ebb60daee4a02b9317b9a1
GripenANM <titanrtx2080ti@gmail.com>
2023-10-02 13:45:52 -0700

    EDIT added type and make it robust

--- python/helper/pdf2Token.py
+++ python/helper/pdf2Token.py
@@ -2,18 +2,17 @@ from PyPDF2 import PdfReader
 import spacy
 
 
-def pdf2Tokens(fpath):
+def pdf2Tokens(fpath: str) -> list:
     Tokens = []
     nlp = spacy.load("en_core_web_sm")
     pdf = PdfReader(fpath)
     for i in range(len(pdf.pages)):
         page = pdf.pages[i]
         text = page.extract_text()
-        doc = nlp(text)
-        Tokens = Tokens + list(doc)
-        print(text)
-    print(Tokens)
+        if text:
+            doc = nlp(text)
+            Tokens = Tokens + list(doc)
     return Tokens
 
 
-print(type(pdf2Tokens('F:/swe-resume-evaluator/python/resources/sample_resume.pdf')))
+print(pdf2Tokens('F:/swe-resume-evaluator/python/resources/sample_resume.pdf'))
