060cdb5e9105fa6e2011fc5fa5fd58eb78a143ff
GripenANM <titanrtx2080ti@gmail.com>
2023-10-02 13:47:21 -0700

    ADD documented the code and minor clean up

--- python/helper/pdf2Token.py
+++ python/helper/pdf2Token.py
@@ -1,3 +1,10 @@
+# This function loads spacy and uses the en_core_web_sm model,
+# which is a small English language model that includes a vocabulary,
+# a binary model, and a set of rules. It then creates an empty list called Tokens,
+# and then iterates through the pages of the pdf file, extracting the text from each page.
+# If text is extracted, it creates a doc object that contains the text,
+# and then adds the doc to the list Tokens. The function then returns the list Tokens.
+
 from PyPDF2 import PdfReader
 import spacy
 
