f28a34a7f4776e62a43d14a5b5c37759de4ff1a3
jiin <jiin.kim109@gmail.com>
2023-10-03 12:34:49 -0700

    add a logo component test assert

--- src/__tests__/components/Logo.test.jsx
+++ src/__tests__/components/Logo.test.jsx
@@ -6,6 +6,7 @@ describe('Logo Component', () => {
     it('should display an image for a given width and height, which are passed through its props', () => {
         render(<Logo width={200} height={300} />);
         const image = screen.getByAltText('logo');
+        expect(image.src).toContain('logo.png');
         expect(image).toHaveStyle('width: 200px');
         expect(image).toHaveStyle('height: 300px');
     });
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import logo from '../img/logo.png';
+import logo from '../resources/logo.png';
 
 const Image = styled.img`
     width: ${(props) => props.width}px;
