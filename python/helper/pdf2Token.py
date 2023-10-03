1ce2bcb5790bdfa7979a54e252ab4beb3a230766
GripenANM <titanrtx2080ti@gmail.com>
2023-10-02 23:20:12 -0700

    ADD Remove white/double space in token list

--- python/helper/pdf2Token.py
+++ python/helper/pdf2Token.py
@@ -1,18 +1,29 @@
-# This code uses the Tika library to parse a PDF file and extract the text from the file.
-#  It then uses the spaCy library to tokenize the text and return a list of tokens.
+# This code parses the PDF file and extracts the text
+# It then tokenizes the text with spacy and removes empty spaces
+# The tokens are then returned as a list
 
 from tika import parser
 import spacy
+import re
 
 
-def pdf2Tokens(fpath: str) -> list:
-    # Load the English language model in spacy
-    nlp = spacy.load("en_core_web_sm")
+# Load the English language model in spacy
+nlp = spacy.load("en_core_web_sm")
 
-    # Parse the PDF file and extract the text
-    raw = parser.from_file(fpath)
-    text = raw['content'].strip()
+# Parse the PDF file and extract the text
+raw = parser.from_file(fpath)
+text = raw['content'].strip()
 
-    # Tokenize the text
-    doc = nlp(text)
-    return list(doc)
+# Tokenize the text
+doc = list(nlp(text))
+
+# Remove double spaces
+delList = []
+for i in range(len(doc)):
+    if re.match(r"\s\s", doc[i].text):
+        delList.append(i)
+for index in sorted(delList, reverse=True):
+    del doc[index]
+
+# Return the tokens
+return list(doc)
