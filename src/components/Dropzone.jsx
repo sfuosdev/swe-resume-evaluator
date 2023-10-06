28de9528479c1685b7accad47cf48486655d65fd
Daniel Pham <danielpham357@gmail.com>
2023-10-06 16:50:09 -0700

    Update: css styles in for Dropzone component

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -3,15 +3,37 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
     display: flex;
+    padding: 20px;
+    margin: auto;
+    width: 500px;
+    height: 50px;
+    border-size: 1px;
+    border-width: 2px;
+    border-style: dashed;
+    justify-content: center;
+    align-items: center;
 `;
 
+const changeHandler = () => {};
+
+const submitHandler = () => {};
+
+/**
+ * Logo component displays a logo image or an alt text for a width and height given by the props
+ */
 function Dropzone() {
-    <Wrapper>
-        <div>
-            Drop zone here
-            <div>Upload here</div>
-        </div>
-    </Wrapper>;
+    return (
+        <Wrapper>
+            <div>
+                <input type="file" name="file" onChange={changeHandler} />
+                <div>
+                    <button type="button" onClick={submitHandler}>
+                        Upload
+                    </button>
+                </div>
+            </div>
+        </Wrapper>
+    );
 }
 
 export default Dropzone;
