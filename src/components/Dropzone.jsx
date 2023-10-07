99796b25964db0eff936720799da8fc224466de6
Daniel Pham <danielpham357@gmail.com>
2023-10-06 18:08:51 -0700

    Add: Dropzone component - functional upload button

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
     padding: 20px;
     margin: auto;
     width: 500px;
-    height: 50px;
+    height: 100px;
     border-size: 1px;
     border-width: 2px;
     border-style: dashed;
@@ -14,23 +14,56 @@ const Wrapper = styled.div`
     align-items: center;
 `;
 
-const changeHandler = () => {};
-
-const submitHandler = () => {};
-
 /**
- * Logo component displays a logo image or an alt text for a width and height given by the props
+ * Dropzone component let users upload their files through the upload button
  */
 function Dropzone() {
+    // Track uploaded file object to be passed to handleFile Prop
+    const [uploadedFile, setUploadedFile] = useState({});
+    // Track uploaded file details to be displayed to users
+    const [fileDetailsDropdown, setFileDetailsDropdown] = useState(false);
+
+    // Reference to the hidden file input element
+    const hiddenFileInputReference = useRef(null);
+
+    // When the upload button is clicked, the choose file button in the hidden file input element will be clicked
+    const uploadFileHandler = () => {
+        hiddenFileInputReference.current.click();
+    };
+
+    // After user upload a file from their device, update state data and handle request to backend
+    const changeFileHandler = (event) => {
+        setUploadedFile(event.target.files[0]);
+        setFileDetailsDropdown(true);
+    };
+
+    const submitHandler = () => {
+        console.log(uploadedFile);
+    };
+
     return (
         <Wrapper>
             <div>
-                <input type="file" name="file" onChange={changeHandler} />
                 <div>
-                    <button type="button" onClick={submitHandler}>
+                    <button type="button" onClick={uploadFileHandler}>
                         Upload
                     </button>
                 </div>
+                {fileDetailsDropdown && (
+                    <div>
+                        <p>Filename: {uploadedFile.name}</p>
+                        <button type="button" onClick={submitHandler}>
+                            Evaluate
+                        </button>
+                    </div>
+                )}
+                <input
+                    type="file"
+                    name="file"
+                    onChange={changeFileHandler}
+                    ref={hiddenFileInputReference}
+                    style={{ display: 'none' }} // Make the file input element invisible
+                />
             </div>
         </Wrapper>
     );
