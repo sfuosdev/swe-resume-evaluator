8df1bbf3f3ad26a5c58ebdbb4bc9c3f33e962275
jiin <jiin.kim109@gmail.com>
2023-10-26 15:47:41 -0700

    add dragdropIndicator story

---
+++ src/components/DragAndDropIndicator.stories.js
@@ -0,0 +1,18 @@
+import DragAndDropIndicator from './DragAndDropIndicator';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'DragAndDropIndicator',
+    component: DragAndDropIndicator,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'centered',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary = {
+    args: {},
+};
