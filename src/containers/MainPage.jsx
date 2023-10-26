655a798923a39aad1589693a32c9d1a48c4d05ef
Seunghwan Kim <seunghwan_kim@sfu.ca>
2023-10-26 15:44:02 -0700

    return MainPage to original

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Logo from '../components/Logo';
-import DragAndDropIndicator from '../components/DragAndDropIndicator';
 
 function MainPage() {
     return (
         <div>
             <Header />
             <Logo width={250} height={250} />
-            <DragAndDropIndicator />
             <Footer />
         </div>
     );
