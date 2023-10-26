1b94e3859f7efefa510d387b1e44dd5b138f4ff1
Seunghwan Kim <seunghwan_kim@sfu.ca>
2023-10-25 23:43:17 -0700

    ProgressText component implemented

--- src/__tests__/components/ProgressText.text.jsx
+++ src/__tests__/components/ProgressText.text.jsx
@@ -2,3 +2,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ProgressText from '../../components/ProgressText';
+
+describe('ProgressText Component', () => {
+    it('should render the component when loading == true', () => {
+        render(<ProgressText loading />);
+        expect(screen.getByText('This is the 1st line...')).toBeInTheDocument();
+    });
+
+    it('should not render the component when loading == false', () => {
+        render(<ProgressText loading={false} />);
+        expect(screen.queryByText('This is the 1st line...')).toBeNull();
+    });
+});
--- src/components/ProgressText.jsx
+++ src/components/ProgressText.jsx
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types';
 import styled, { keyframes } from 'styled-components';
 
 const fadeOutToTop = keyframes`
-  from { opacity: 0.5; }
-  to {
-    opacity: 0;
-    transform: translateY(-100%);
+  from { 
+    opacity: 0.3;
+    transform: translateY(100%);
   }
+  to { opacity: 0; }
 `;
 
 const moveToCompleted = keyframes`
@@ -17,12 +17,12 @@ const moveToCompleted = keyframes`
     opacity: 1;
     transform: translateY(100%);
   }
-  to { opacity: 0.5 }
+  to { opacity: 0.3 }
 `;
 
 const moveToScheduled = keyframes`
   from {
-    opacity: 0.5;
+    opacity: 0.3;
     transform: translateY(100%);
   }
   to { opacity: 1 }
@@ -33,12 +33,19 @@ const fadeInFromBottom = keyframes`
     opacity: 0;
     transform: translateY(100%);
   }
-  to { opacity: 0.5 }
+  to { opacity: 0.3 }
 `;
 
 const LineContainer = styled.div`
-    animation: ${({ animation }) => animation} 2s;
+    animation: ${({ animation }) => animation} 3s;
+    opacity: 0;
     display: ${({ hide }) => (hide ? 'none' : 'block')};
+    font-family: arial, sans-serif;
+    font-size: 40px;
+    font-weight: bold;
+    color: #333;
+    text-align: center;
+    padding: 10px;
 `;
 
 /**
@@ -50,29 +57,30 @@ const LineContainer = styled.div`
 function ProgressText({ loading = true }) {
     // eslint-disable-next-line no-undef, prettier/prettier
     const textLines = useMemo(() => [
-            '',
-            'This is the 1st line.',
-            'This is the 2nd line.',
-            'This is the 3rd line.',
-            'This is the 4th line.',
-            'This is the 5th line.',
-            'This is the 6th line.',
-            'This is the 7th line.',
-            'This is the 8th line.',
-            'This is the 9th line.',
-            'This is the 10th line.',
-            'This is the 11th line.',
-            'This is the 12th line.',
-            'This is the 13th line.',
-            'This is the 14th line.',
-            'This is the 15th line.',
+            '\u200B', // Zero-Width Space (invisible)
+            '\u200B',
+            'This is the 1st line...',
+            'This is the 2nd line...',
+            'This is the 3rd line...',
+            'This is the 4th line...',
+            'This is the 5th line...',
+            'This is the 6th line...',
+            'This is the 7th line...',
+            'This is the 8th line...',
+            'This is the 9th line...',
+            'This is the 10th line...',
+            'This is the 11th line...',
+            'This is the 12th line...',
+            'This is the 13th line...',
+            'This is the 14th line...',
+            'This is the 15th line...',
             // Add more lines as needed
-            '',
+            '\u200B ',
         ],
         [],
     );
 
-    const [currentIndex, setCurrentIndex] = useState([0, 1, 2]);
+    const [currentIndex, setCurrentIndex] = useState([0, 1, 2, 3]);
 
     // eslint-disable-next-line consistent-return
     useEffect(() => {
@@ -80,8 +88,10 @@ function ProgressText({ loading = true }) {
             if (currentIndex[2] < textLines.length - 2) {
                 const nextIndex = currentIndex.map((index) => index + 1);
                 setCurrentIndex(nextIndex);
+            } else {
+                setCurrentIndex([0, 1, 2, 3]);
             }
-        }, 2000); // Adjust the delay as needed
+        }, 2900); // Adjust the delay as (animation delay - 100)
 
         return () => clearInterval(interval);
     }, [currentIndex, textLines]);
