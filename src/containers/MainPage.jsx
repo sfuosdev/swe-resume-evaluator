38b9d3946bcd221e63dbcbea50ccb3de12563ff3
Jiin Kim <jiinkim@d207-023-186-144.wireless.sfu.ca>
2023-11-01 15:47:02 -0700

    install react router

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Logo from '../components/Logo';
 import Dropzone from '../components/Dropzone';
 
 function MainPage() {
     return (
         <div>
-            <Header />
             <Logo width={250} height={250} />
             <Dropzone />
-            <Footer />
         </div>
     );
 }
