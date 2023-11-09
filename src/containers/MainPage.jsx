b575be3b92fd19d71481af63cf0e398875f0f8b3
Seunghwan Kim <seunghwan_kim@sfu.ca>
2023-11-09 05:06:54 -0800

    before pull

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Logo from '../components/Logo';
+import DragAndDropIndicator from '../components/DragAndDropIndicator';
+import ProgressText from '../components/ProgressText';
 
 function MainPage() {
     return (
         <div>
             <Header />
             <Logo width={250} height={250} />
+            <ProgressText loading />
+            <DragAndDropIndicator />
             <Footer />
         </div>
     );
